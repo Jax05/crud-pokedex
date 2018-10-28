022a906fa0d096736febefd7181e8a128b49b7a4
Jax05 <carley.tripp@outlook.com>
2018-10-27 20:24:42 -0500

    Create helper methods to check current user and if a user is logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,16 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  helpers do
+    
+    def current_user
+      @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?
+      !!current_user
+    end
+
+  end
 end
