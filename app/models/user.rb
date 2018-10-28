7df6b3a9b308daa620a332800be72eccb4f36d08
Jax05 <carley.tripp@outlook.com>
2018-10-27 20:59:26 -0500

    Add slug and find_by_slug methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :username, presence: true, uniqueness: true
   has_many :pokemon
+
+  def slug
+    self.username.split(" ").join("-")
+  end
+
+  def self.find_by_slug(slug)
+    username = slug.split("-").join(" ")
+    User.find_by_username(username)
+  end
 end
