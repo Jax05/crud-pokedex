ccebf5f641cba0021012e8c0ece4a9adac735ab1
Jax05 <carley.tripp@outlook.com>
2018-10-27 20:34:56 -0500

    Add logic to set user id equal to session's user id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   post '/signup' do
     @user = User.new(params)
     if @user.save
+      session[:user_id] = @user.id
       redirect to '/pokemon'
     else
       redirect to '/signup'
