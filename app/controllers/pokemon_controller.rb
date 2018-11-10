a395153a0187c82cf53e23489bfcf7b639cde9a4
Jax05 <carley.tripp@outlook.com>
2018-11-09 21:53:49 -0600

    Add logic to create Pokemon on form submit

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -6,4 +6,13 @@ class PokemonController < ApplicationController
   get '/pokemon/new' do
     erb :'pokemon/new'
   end
+
+  post '/pokemon' do
+    @pokemon = current_user.pokemon.build(params)
+    if @pokemon.save
+      redirect to '/pokemon'
+    else
+      redirect to '/pokemon/new'
+    end
+  end
 end
