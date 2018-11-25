6a0cf1fd25d45ba87ce5b951bc321636d6f1a159
Jax05 <carley.tripp@outlook.com>
2018-11-24 22:55:48 -0600

    Get rack-flash working and allow it to sweep stale messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
+require 'sinatra/base'
+require 'rack-flash'
+
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
     set :session_secret, "crud-pokedex-application"
+
+    use Rack::Flash, :sweep => true # clears stale flash entries
   end
 
   get '/' do
@@ -11,7 +16,7 @@ class ApplicationController < Sinatra::Base
   end
 
   helpers do
-    
+
     def current_user
       @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
     end
