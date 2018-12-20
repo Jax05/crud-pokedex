c722dceebbb10487ce6cebd108005939ab0060e0
Jax05 <carley.tripp@outlook.com>
2018-12-20 15:50:42 -0600

    Add a seed file that creates a user and three Pokemon

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+User.create(username: "Trainer", password: "c4tchem4all")
+
+@user = User.find_by_username("Trainer")
+
+@user.pokemon.create(name: "Totodile", type_name: "Water", description: "a Pokemon with powerful jaws")
+@user.pokemon.create(name: "Chikorita", type_name: "Grass", description: "a Pokemon that waves its leaf around to keep foes at bay")
+@user.pokemon.create(name: "Eevee", type_name: "Normal", description: "a Pokemon with an unstable genetic makeup")
